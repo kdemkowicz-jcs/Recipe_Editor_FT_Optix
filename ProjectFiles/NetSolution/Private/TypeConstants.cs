3d955a457261ba5affe870aaf5b28d17bdfc2622
Katrina Demkowicz <kdemkowicz@jcs.com>
2025-09-04 09:58:30 -0400

    Making a fresh repo

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,37 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace Recipe_Editor
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Recipe_Editor");
+        public static readonly NodeId Configuration = new NodeId(namespaceIndex, new Guid("9cf154c923572624320f16c16fa657a3"));
+        public static readonly NodeId NewConnectionCredentialsPanel = new NodeId(namespaceIndex, new Guid("d8a8bb0d6c0490587ebdf9c1eb42375e"));
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("01a7e8933afd7c95fe894b0859308138"));
+        public static readonly NodeId RequestAssistanceDisabledPanel = new NodeId(namespaceIndex, new Guid("f0afd51b3f49febd40b32153974e81b8"));
+        public static readonly NodeId DeviceSettingsWidget = new NodeId(namespaceIndex, new Guid("343e9a553a1648e3de95b91f6d877b43"));
+        public static readonly NodeId DateAndTimeWidget = new NodeId(namespaceIndex, new Guid("ba060dabe3223c9b73ecd35e8a6579f1"));
+        public static readonly NodeId Product_Information = new NodeId(namespaceIndex, new Guid("548a589cc1532b072d9d24316f7b6090"));
+        public static readonly NodeId NetworkSettingsWidget = new NodeId(namespaceIndex, new Guid("93cbf19dd68a72c543a41f03c3c2b7a1"));
+        public static readonly NodeId FTRemoteAccessWidget = new NodeId(namespaceIndex, new Guid("41382c57dc616862c1af276ff6942abc"));
+        public static readonly NodeId WAN = new NodeId(namespaceIndex, new Guid("d17d8ab3d7f8047c35a02cbe0b7248c6"));
+        public static readonly NodeId LAN = new NodeId(namespaceIndex, new Guid("41f433c486aa6dfdb14c2c47570eea1a"));
+        public static readonly NodeId Diagnostics = new NodeId(namespaceIndex, new Guid("632709bc0ca708c922da2f68cb20eb09"));
+        public static readonly NodeId FTRemoteAccessWidgetDataObject = new NodeId(namespaceIndex, new Guid("07b10933e4ee4d4cbf84518e190ea9be"));
+        public static readonly NodeId ConnectionPanel = new NodeId(namespaceIndex, new Guid("e348b14dbc7035688714c7ced8a72128"));
+        public static readonly NodeId AcceptDenyIncomingRequestPanel = new NodeId(namespaceIndex, new Guid("c05a81edcb91fa074772f19543ea1b38"));
+        public static readonly NodeId RequestAssistancePanel = new NodeId(namespaceIndex, new Guid("77122192dcccfaa39d1e78d7bd27e222"));
+        public static readonly NodeId RequestAssistanceEnabledPanel = new NodeId(namespaceIndex, new Guid("dc2c41b238ed9ce9aa4f98d28d04b528"));
+        public static readonly NodeId EmptyPanel = new NodeId(namespaceIndex, new Guid("323ecbd80fe0dccc66307959b261f54a"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("Recipe_Editor");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,91 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "9cf154c923572624320f16c16fa657a3")]
+public class Configuration : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
+public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "f0afd51b3f49febd40b32153974e81b8")]
+public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "343e9a553a1648e3de95b91f6d877b43")]
+public class DeviceSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "ba060dabe3223c9b73ecd35e8a6579f1")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "548a589cc1532b072d9d24316f7b6090")]
+public class Product_Information : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "93cbf19dd68a72c543a41f03c3c2b7a1")]
+public class NetworkSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "41382c57dc616862c1af276ff6942abc")]
+public class FTRemoteAccessWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
+public class WAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
+public class LAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "632709bc0ca708c922da2f68cb20eb09")]
+public class Diagnostics : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "e348b14dbc7035688714c7ced8a72128")]
+public class ConnectionPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "c05a81edcb91fa074772f19543ea1b38")]
+public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
+public class RequestAssistancePanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
+public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "Recipe_Editor", Guid = "323ecbd80fe0dccc66307959b261f54a")]
+public class EmptyPanel : FTOptix.UI.Panel
+{
+}
